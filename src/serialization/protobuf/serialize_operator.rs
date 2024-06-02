ebd75b0a5a54d6df5850b205760682487d19d63f
Magnus Kroner <magnus.kroner@gmail.com>
2024-06-02 16:31:35 +0200

    started serialization

--- src/serialization/protobuf/serialize_operator.rs
+++ src/serialization/protobuf/serialize_operator.rs
@@ -1,24 +1,36 @@
-use crate::query::operator::Operator;
+use crate::query::{
+    operator::Operator,
+    window::{aggregation::{Aggregation, AggregationType}, window_descriptor::WindowDescriptor},
+};
 use prost_types::Any;
 
 use super::nes::{
-    serializable_operator::{source_details::SerializableLogicalSourceDescriptor, SourceDetails},
+    serializable_operator::{
+        source_details::SerializableLogicalSourceDescriptor,
+        window_details::{self, aggregation},
+        SourceDetails, WindowDetails,
+    },
     SerializableOperator,
 };
 
 pub fn serialize_operator_details(operator: &Operator) -> prost_types::Any {
     match operator {
         Operator::LogicalSource { source_name } => {
-            Any::from_msg(&logical_source_details(source_name.clone())).unwrap()
+            Any::from_msg(&logical_source_details(source_name)).unwrap()
         }
         Operator::Filter { expression, .. } => todo!(),
-        Operator::Window { descriptor, .. } => todo!(),
+        Operator::Window {
+            descriptor,
+            aggregations,
+            key_fields,
+            ..
+        } => Any::from_msg(&window_details(descriptor, aggregations, key_fields)).unwrap(),
     }
 }
 
-pub fn logical_source_details(source_name: String) -> SourceDetails {
+pub fn logical_source_details(source_name: &String) -> SourceDetails {
     let descriptor = SerializableLogicalSourceDescriptor {
-        logical_source_name: source_name,
+        logical_source_name: source_name.to_string(),
         ..Default::default()
     };
     let descriptor = Any::from_msg(&descriptor).unwrap();
@@ -28,12 +40,52 @@ pub fn logical_source_details(source_name: String) -> SourceDetails {
     }
 }
 
+pub fn window_details(
+    descriptor: &WindowDescriptor,
+    aggregations: &[Aggregation],
+    key_field: &[String],
+) -> WindowDetails {
+    WindowDetails {
+        window_type: todo!(),
+        window_aggregations: serialize_aggregations(aggregations),
+        distr_char: todo!(),
+        keys: todo!(),
+        allowed_lateness: todo!(),
+        origin: todo!(),
+    }
+}
+
+pub fn serialize_aggregations(aggregations: &[Aggregation]) -> Vec<window_details::Aggregation> {
+    aggregations
+        .iter()
+        .map(|agg| window_details::Aggregation {
+            r#type: serialize_aggregation_type(agg.agg_type).into(),
+            on_field: todo!(),
+            as_field: None,
+        })
+        .collect()
+}
+
+pub const fn serialize_aggregation_type(agg_type: AggregationType) -> window_details::aggregation::Type {
+    use window_details::aggregation::Type as T;
+    use AggregationType as AT;
+    match agg_type {
+        AT::Sum => T::Sum,
+        AT::Average => T::Avg,
+        AT::Min => T::Min,
+        AT::Max => T::Max,
+        AT::Median => T::Median,
+        AT::Count => T::Count,
+    }
+}
+
 #[derive(Debug, Default, Clone)]
 pub struct SerializableOperatorBuilder {
     details: Option<Any>,
     operator_id: Option<u64>,
     children_ids: Vec<u64>,
 }
+
 impl SerializableOperatorBuilder {
     pub fn new() -> Self {
         Self {
